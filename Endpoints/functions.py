9790de5a2fcf640342f6f623a14f5aadbf4476df
globaldusk <67882245+globaldusk@users.noreply.github.com>
2023-03-08 11:32:42 +1100

    finished just needs to be cleaned

--- Endpoints/functions.py
+++ Endpoints/functions.py
@@ -1,36 +1,6 @@
-import requests
-import datetime
-
-import json
-import os
-import urllib.request as urlb
-
 # api key is 459a67bef104c5a557e66c0fb1ea11bf
 
 apiKey = "459a67bef104c5a557e66c0fb1ea11bf"
-# lat = "54.4609"
-# lon = "3.0886"
-country = "GB"
-state = "england"
-# coordURL = "https://api.openweathermap.org/geo/1.0/reverse?lat=" + lat + "&lon=" + lon + "&limit=5&appid=" + apiKey
-# nameURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,
-# daily&appid=" + apiKey
-
-# print(coordURL)
-
-data = ""
-lines = ""
-
-# response = requests.get(coordURL)
-# print(response.status_code)
-# print(response.json())
-
-# response_url = urlb.urlopen('https://api.publicapis.org/entries')
-# print(response_url)
-# a = (requests.get("https://api.openweathermap.org/geo/1.0/reverse?lat=54.4609&lon=-3.0886&limit=5&appid"
-#                  "=459a67bef104c5a557e66c0fb1ea11bf"))
-# print(a.json())
-
 
 # read data from file
 def read_data_from_file(file):
@@ -38,6 +8,7 @@ def read_data_from_file(file):
         return f.read().split('/')
 
 
+#get weather Json from API link
 def get_weather_data(lat, lon):
     url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey
     url = url.strip()
@@ -45,15 +16,6 @@ def get_weather_data(lat, lon):
 
     return url
 
-
-# request data from API
-def get_weather_by_name(name, country, state):
-    url = 'http://api.openweathermap.org/geo/1.0/direct?q='+name+','+state+','+country+'&limit=5&APPID=' + apiKey
-    print (url)
-    response = requests.get(url)
-    return response.json()
-    # sort from json to data
-    # return data
 
 
 # send data to file
--- Endpoints/main.py
+++ Endpoints/main.py
@@ -1,37 +1,41 @@
+import requests
+
 from functions import *
 
 # api key is 459a67bef104c5a557e66c0fb1ea11bf
 
 apiKey = "459a67bef104c5a557e66c0fb1ea11bf"
-lat = ""
-long = ""
-name = ""
-currentCondition = ""
-temp = ""
+
 linesToWrite = []
 
-weatherJson = ""
 
 lines = read_data_from_file("locations.txt")
 
 for line in lines:
-
+    # breaks the coordinates into lat and lon
     line = line.split(',')
-
     lat = line[0]
     lon = line[1]
 
+    # less typing by using .session()
     with requests.session() as s:
+        # uses method from functions file
         s.get(get_weather_data(lat, lon))
 
+        # the raw Json
         data = s.get(get_weather_data(lat, lon)).json()
 
+        # printing to test the output
         print(data)
         name = data['city']['name']
         data = data['list'][0]
+
+        # printing to test the output
         print(data)
         currentCondition = data['weather'][0]['main']
         temp = data['main']['temp']
-        weatherString = (name+": the weather is "+currentCondition+" and the temp is "+str(temp)+"\n")
+
+        # writing all the data to user-friendly format
+        weatherString = (name + ": the weather is " + currentCondition + " and the temp is " + str(temp) + "\n")
         linesToWrite.append(weatherString)
         write_weather_to_file(linesToWrite, 'weather.txt')
